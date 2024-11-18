71a8dcaffd2ca1d1d903b2d4b69011e8a9532b54
ldingtao <ldingtao@163.com>
2024-11-18 22:18:54 +0800

    feat(init): init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+import localFont from "next/dist/compiled/@next/font/dist/local";
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({weight: ['400','700'], subsets: ['latin']});
